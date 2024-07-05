b241655f0352e93848d6dc826f848ba585375d05
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-07-05 11:37:42 +0200

    Move method implementations into cpp file

--- App/VektoriaApp/building.h
+++ App/VektoriaApp/building.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "Vektoria/Scene.h"
-#include "Vektoria/Placement.h"
+#include "Vektoria/GeoCylinder.h"
+#include "Vektoria/GeoTube.h"
 #include "Vektoria/GeoWall.h"
+#include "Vektoria/Placement.h"
+#include "Vektoria/Scene.h"
 
 class Building
 {
@@ -42,97 +44,7 @@ public:
 	void setRoofMaterial(Vektoria::CMaterial* mat) { m_roofMat = mat; }
 	void setDoorFrameMaterial(Vektoria::CMaterial* mat) { m_doorFrameMat = mat; }
 
-	void initialize(Vektoria::CScene& scene, const Vektoria::CHVector& pos = {})
-	{
-		// Floor
-		float innerRadius = m_params.baseOuterRadius - m_params.baseThickness;
-		m_buildingFloor.SetTextureRepeat(10.0f, 10.0f);
-		m_buildingFloor.InitStraight(innerRadius, m_params.baseOuterRadius, m_params.outerfloorHeight, m_buildingFloorMat, 100);
-		m_buildingFloorPlace.AddGeo(&m_buildingFloor);
-		m_buildingFloorPlace.TranslateY(0.5f * m_params.outerfloorHeight);
-
-		m_innerFloor.SetTextureRepeat(20.0f, 20.0f);
-		m_innerFloor.Init(innerRadius, m_params.innerFloorHeight, m_innerFloorMat, 100);
-		m_innerFloorPlace.AddGeo(&m_innerFloor);
-
-		// Walls
-		m_baseWallPlace.TranslateY(m_params.outerfloorHeight - 0.001f);
-
-		// Door	
-		m_innerDoor.InitOvalArc(1.0f - m_params.doorFrameWidth, 1.0f - m_params.doorFrameHeight, false);
-
-		m_doorFrame.AddGeoWindow(&m_innerDoor, 0.5f * m_params.doorFrameWidth, 0.0f);
-		m_doorFrame.Init(1.0f, 1.0f, m_params.doorFrameThickness, m_doorFrameMat, 
-			false, false, false, false, true, true);
-
-		Vektoria::CHMat doorFrameMat;
-		doorFrameMat.TranslateZ(0.5f * m_params.wallThickness);
-		m_doorFrame.Transform(doorFrameMat);
-
-		m_door.AddGeoWall(&m_doorFrame);
-		m_door.InitOvalArc(m_params.doorWidth, m_params.doorHeight, true);
-
-		// Wall Geo
-		float halfWallLength = m_params.baseOuterRadius * PI; // From: Circumference = 2 * pi * r
-		int halfDoorCount = static_cast<int>(0.5f * m_params.doorCount);
-
-		for (int i = 0; i < halfDoorCount; i++)
-		{
-			float doorPosX = i * (halfWallLength / halfDoorCount);
-			m_baseWall.AddGeoWindow(&m_door, doorPosX, 0.0f);
-		}
-
-		m_baseWall.SetRoundingX(PI, m_params.roundingResolution);
-		m_baseWall.SetTextureRepeat(10.0f, 10.0f);
-		m_baseWall.Init(halfWallLength, m_params.baseHeight, m_params.wallThickness, m_wallMat,
-			false, false, true, false, true, true);
-
-		// Outer Wall
-		m_outerWallPlaceLeft.RotateY(PI);
-		m_outerWallPlaceLeft.AddGeo(&m_baseWall);
-
-		m_outerWallPlaceRight.AddGeo(&m_baseWall);
-
-		// Inner Wall
-		float innerWallScale = (m_params.baseOuterRadius - m_params.baseThickness + m_params.wallThickness) / m_params.baseOuterRadius;
-		m_innerWallPlaceLeft.RotateY(PI);
-		m_innerWallPlaceLeft.ScaleDelta(innerWallScale, 1.0f, innerWallScale);
-		m_innerWallPlaceLeft.AddGeo(&m_baseWall);
-
-		m_innerWallPlaceRight.ScaleDelta(innerWallScale, 1.0f, innerWallScale);
-		m_innerWallPlaceRight.AddGeo(&m_baseWall);
-
-		// Roof
-		float roofCircumference = 0.5f * m_params.baseThickness * PI;
-		float midRadius = m_params.baseOuterRadius - (0.5f * m_params.baseThickness);
-
-		m_roof.SetRoundingY(PI, m_params.roundingResolution);
-		m_roof.SetTextureRepeat(20.0f, 10.0f);
-		m_roof.Init(1.0f, roofCircumference + 0.01f, m_params.wallThickness + 0.01f, m_roofMat,
-			false, false, true, true, true, true);
-
-		m_roof.Subdivide(0.01f);
-		m_roof.BendX(midRadius, TWOPI);
-
-		m_roofPlace.TranslateY(m_params.baseHeight - 0.001f);
-		m_roofPlace.RotateYDelta(PI);
-		m_roofPlace.AddGeo(&m_roof);
-
-		// Hierachy
-		scene.AddPlacement(&m_place);
-		m_place.Translate(pos);
-		m_place.AddPlacement(&m_baseFloorPlace);
-
-		m_baseFloorPlace.AddPlacement(&m_buildingFloorPlace);
-		m_baseFloorPlace.AddPlacement(&m_innerFloorPlace);
-		m_baseFloorPlace.AddPlacement(&m_baseWallPlace);
-
-		m_baseWallPlace.AddPlacement(&m_outerWallPlaceLeft);
-		m_baseWallPlace.AddPlacement(&m_outerWallPlaceRight);
-		m_baseWallPlace.AddPlacement(&m_innerWallPlaceLeft);
-		m_baseWallPlace.AddPlacement(&m_innerWallPlaceRight);
-		m_baseWallPlace.AddPlacement(&m_roofPlace);
-	}
+	void initialize(Vektoria::CScene& scene, const Vektoria::CHVector& pos = {});
 
 private:
 	Building::Parameters m_params;
@@ -155,13 +67,10 @@ private:
 	// Geos
 	Vektoria::CGeoTube m_buildingFloor;
 	Vektoria::CGeoCylinder m_innerFloor;
-
 	Vektoria::CGeoWall m_doorFrame;
 	Vektoria::CGeoWindow m_innerDoor;
-
 	Vektoria::CGeoWindow m_door;
 	Vektoria::CGeoWall m_baseWall;
-
 	Vektoria::CGeoWall m_roof;
 
 	// Materials
---
+++ App/VektoriaApp/road.cpp
@@ -0,0 +1,159 @@
+#include "road.h"
+
+void Road::initialize(Vektoria::CScene& scene, const Vektoria::CHVector& pos)
+{
+	scene.AddPlacement(&m_place);
+	m_place.Translate(pos);
+
+	// Hill side
+	Vektoria::CHMats hillMatPointers;
+	Vektoria::CHMat baseMat, topMat;
+	baseMat.Scale(m_params.outerHillRadius, 0.0f, m_params.outerHillRadius);
+	topMat.Scale(m_params.innerHillRadius - 0.01f, 0.0f, m_params.innerHillRadius - 0.01f);
+	topMat.TranslateYDelta(m_params.hillHeight);
+	hillMatPointers.Add(&baseMat);
+	hillMatPointers.Add(&topMat);
+
+	m_hillOutline.InitCircle(1.0f, 4 * m_params.quarterCircleSteps);
+	m_hillSweep.SetTextureRepeat(10.0f, 0.5f);
+	m_hillSweep.InitOutline(m_hillOutline, hillMatPointers, m_areaMat);
+	m_hillPlace.AddGeo(&m_hillSweep);
+	m_place.AddPlacement(&m_hillPlace);
+
+	// Infrastructure
+	float radius = m_params.roundaboutRadius;
+	auto wayType = m_params.wayType;
+	auto crossingType = m_params.crossingType;
+
+	m_infra.SetMaterialForWay(wayType, m_roadMat);
+	m_infra.SetMaterialForCrossing(crossingType, m_crossingMat);
+
+	auto crossing1 = m_infra.MakeCrossing(radius, 0.0f, 0.0f);
+	auto crossing2 = m_infra.MakeCrossing(0.0f, 0.0f, radius);
+	auto crossing3 = m_infra.MakeCrossing(-radius, 0.0f, 0.0f);
+	auto crossing4 = m_infra.MakeCrossing(0.0f, 0.0f, -radius);
+
+	float mappingContraction = 0.1f;
+	crossing1->SetMappingContraction(mappingContraction);
+	crossing2->SetMappingContraction(mappingContraction);
+	crossing3->SetMappingContraction(mappingContraction);
+	crossing4->SetMappingContraction(mappingContraction);
+
+	auto way1 = m_infra.MakeWay(crossing1, crossing2, wayType);
+	auto way2 = m_infra.MakeWay(crossing2, crossing3, wayType);
+	auto way3 = m_infra.MakeWay(crossing3, crossing4, wayType);
+	auto way4 = m_infra.MakeWay(crossing4, crossing1, wayType);
+
+	float junctionLength = 0.2f * radius;
+	crossing1->SetJunctionLengthAll(junctionLength);
+	crossing3->SetJunctionLengthAll(junctionLength);
+	crossing2->SetJunctionLengthAll(junctionLength);
+	crossing4->SetJunctionLengthAll(junctionLength);
+
+	m_infra.CreateNet();
+
+	auto areaInner = m_infra.MakeArea(way1, crossing1, m_areaMat);
+	areaInner->SetUV(20.0f, 20.0f);
+
+	Vektoria::CHVectors hemline1;
+	hemline1.AddList(crossing1->GetHemlineRight(way1));
+	hemline1.AddList(way1->GetHemlineLeft());
+	addQuarterCircle(hemline1, m_params.innerHillRadius, 0.0f, HALFPI, -1.0f);
+
+	Vektoria::CHVectors hemline2;
+	hemline2.AddList(crossing2->GetHemlineRight(way2));
+	hemline2.AddList(way2->GetHemlineLeft());
+	addQuarterCircle(hemline2, m_params.innerHillRadius, 0.0f, PI, -1.0f);
+
+	Vektoria::CHVectors hemline3;
+	hemline3.AddList(crossing3->GetHemlineRight(way3));
+	hemline3.AddList(way3->GetHemlineLeft());
+	addQuarterCircle(hemline3, m_params.innerHillRadius, 0.0f, PI + HALFPI, -1.0f);
+
+	Vektoria::CHVectors hemline4;
+	hemline4.AddList(crossing4->GetHemlineRight(way4));
+	hemline4.AddList(way4->GetHemlineLeft());
+	addQuarterCircle(hemline4, m_params.innerHillRadius, 0.0f, 0.0f, -1.0f);
+
+	auto areaOuter1 = m_infra.MakeArea(hemline1, m_areaMat);
+	areaOuter1->SetUV(20.0f, 20.0f);
+
+	auto areaOuter2 = m_infra.MakeArea(hemline2, m_areaMat);
+	areaOuter2->SetUV(20.0f, 20.0f);
+
+	auto areaOuter3 = m_infra.MakeArea(hemline3, m_areaMat);
+	areaOuter3->SetUV(20.0f, 20.0f);
+
+	auto areaOuter4 = m_infra.MakeArea(hemline4, m_areaMat);
+	areaOuter4->SetUV(20.0f, 20.0f);
+
+	m_infra.Init();
+	m_infra.TranslateY(m_params.hillHeight);
+	m_place.AddPlacement(&m_infra);
+
+	// Signs
+	m_sign.InitDStop();
+
+	float xOffset = m_params.signOffsetx;
+	float zOffset = m_params.signOffsetz;
+	m_signPlace1.AddGeo(&m_sign);
+	m_signPlace1.Scale(m_params.signScale);
+	m_signPlace1.RotateYDelta(UM_DEG2RAD(315.0f));
+	m_signPlace1.TranslateDelta(radius - xOffset, 0.0f, zOffset);
+	m_infra.AddPlacement(&m_signPlace1);
+
+	m_signPlace2.AddGeo(&m_sign);
+	m_signPlace2.Scale(m_params.signScale);
+	m_signPlace2.RotateYDelta(UM_DEG2RAD(225.0f));
+	m_signPlace2.TranslateDelta(-zOffset, 0.0f, radius - xOffset);
+	m_infra.AddPlacement(&m_signPlace2);
+
+	m_signPlace3.AddGeo(&m_sign);
+	m_signPlace3.Scale(m_params.signScale);
+	m_signPlace3.RotateYDelta(UM_DEG2RAD(135.0f));
+	m_signPlace3.TranslateDelta(-radius + xOffset, 0.0f, -zOffset);
+	m_infra.AddPlacement(&m_signPlace3);
+
+	m_signPlace4.AddGeo(&m_sign);
+	m_signPlace4.Scale(m_params.signScale);
+	m_signPlace4.RotateYDelta(UM_DEG2RAD(45.0f));
+	m_signPlace4.TranslateDelta(zOffset, 0.0f, -radius + xOffset);
+	m_infra.AddPlacement(&m_signPlace4);
+
+
+	m_signPlace5.AddGeo(&m_sign);
+	m_signPlace5.Scale(m_params.signScale);
+	m_signPlace5.RotateYDelta(UM_DEG2RAD(225.0f));
+	m_signPlace5.TranslateDelta(radius - zOffset, 0.0f, -xOffset);
+	m_infra.AddPlacement(&m_signPlace5);
+
+	m_signPlace6.AddGeo(&m_sign);
+	m_signPlace6.Scale(m_params.signScale);
+	m_signPlace6.RotateYDelta(UM_DEG2RAD(135.0f));
+	m_signPlace6.TranslateDelta(xOffset, 0.0f, radius - zOffset);
+	m_infra.AddPlacement(&m_signPlace6);
+
+	m_signPlace7.AddGeo(&m_sign);
+	m_signPlace7.Scale(m_params.signScale);
+	m_signPlace7.RotateYDelta(UM_DEG2RAD(45.0f));
+	m_signPlace7.TranslateDelta(-radius + zOffset, 0.0f, xOffset);
+	m_infra.AddPlacement(&m_signPlace7);
+
+	m_signPlace8.AddGeo(&m_sign);
+	m_signPlace8.Scale(m_params.signScale);
+	m_signPlace8.RotateYDelta(UM_DEG2RAD(315.0f));
+	m_signPlace8.TranslateDelta(-xOffset, 0.0f, -radius + zOffset);
+	m_infra.AddPlacement(&m_signPlace8);
+}
+
+void Road::addQuarterCircle(Vektoria::CHVectors& line, float radius, float height, float startOffset, float direction) const
+{
+	float angleStep = HALFPI / (m_params.quarterCircleSteps - 1);
+	for (int i = 0; i < m_params.quarterCircleSteps; i++)
+	{
+		float angle = startOffset + (angleStep * i * direction);
+		float x = radius * cosf(angle);
+		float z = radius * sinf(angle);
+		line.Make(x, height, z, 1.0f);
+	}
+}
